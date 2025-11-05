31705a03d9d324b85ca2e02b7e0685089ca08073
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-11-05 11:42:02 +0600

    learning node and express

---
+++ src/components/Users.jsx
@@ -0,0 +1,29 @@
+import React, { use } from "react";
+
+const Users = ({ usersPromise }) => {
+  const users = use(usersPromise);
+  console.log(users);
+  return (
+    <div>
+      <div>
+        <form>
+          <h3>Add a user</h3>
+          <input type="text" />
+          <br />
+          <input type="email" name="" id="" />
+          <br />
+          <button>Add user</button>
+        </form>
+      </div>
+      <div>
+        {users.map((user) => (
+          <p key={user.id}>
+            {user.name} Email: {user.email}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Users;
